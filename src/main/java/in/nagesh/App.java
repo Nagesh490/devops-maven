4f1868b5f3154cf02df933b8f19e1cd4097371a0
chotunagesh490@gmail.com <Nagesh>
2024-01-02 22:20:55 +0530

    my 1st commit

---
+++ src/main/java/in/nagesh/App.java
@@ -0,0 +1,13 @@
+package in.nagesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
